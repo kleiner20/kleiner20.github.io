13bc80e136e9675bef8c479aecf47d7de913c768
Keith Kleinschmidt <kleiner_the_airliner.com>
2019-10-04 01:07:48 -0500

    Third Commit

--- homework_3/assets/JavaScript.js
+++ homework_3/assets/JavaScript.js
@@ -2,21 +2,23 @@
       var randomNumbers = [];
       var randomNumbers = [];
       var randomNumbers = [];
-      var randomNumbers = [];
+      
+      var passWord = [];
 
 
-      var passWord = 0;
+      var passWordLength = 0;
       var specialCharacter = false;
       var numericNumber;
       var lowerCase = false;
       var upperCase = false;
       var playButton = document.querySelector("#play");
+      var resultingPassword = "Test One";
 
-      function gatherData() {  
+      function gatherData() {  //Function #1
       do {
-      passWord = prompt("Please provide a password length between 8-128 characters");
+      passWordLength = prompt("Please provide a password length between 8-128 characters");
       }
-      while (passWord < 8 || passWord > 128);
+      while (passWordLength < 8 || passWord > 128);
        
         specialCharacter = confirm("Do you want a special character");
       
@@ -25,14 +27,22 @@
         lowerCase = confirm("Do you want a lower Case letter?");
         
         upperCase = confirm("Do you want a Upper Case letter?");
-      }
+        displayPassword();   
+    }
 
+
+
+// XXX will need to call on this function.
+      function displayPassword() {  //last Function. Displays PW
+        document.getElementById("myTextarea").value = resultingPassword;
+      }
+ //Button 1     
       //Pressing 'Generate Password' is like pressing play
       playButton.addEventListener("click", gatherData);
       
     
       // We then loop through the favTVshows array...
-      for (var j = 0; j < favTVshows.length; j++) {
+      for (var j = 0; j < passWord.length; j++) {
 
         // ...and create alert messages listing out each show in our array.
         alert(favTVshows[j]);
