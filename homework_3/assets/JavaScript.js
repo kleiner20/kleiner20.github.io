db9f85599580cf0c54b0031f72b0e4e4e5b13f81
Keith Kleinschmidt <kleiner_the_airliner.com>
2019-10-05 08:14:58 -0500

    button doesn't work on this commit

--- homework_3/assets/JavaScript.js
+++ homework_3/assets/JavaScript.js
@@ -1,9 +1,10 @@
 
-      var randomNumbers = [];
-      var randomNumbers = [];
-      var randomNumbers = [];
+    //   var rNumbersArray = [];
+      var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+      var sCharacterChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
       
-      var passWord = [];
+      var pwHolder = [];
 
 
       var passWordLength = 0;
@@ -12,9 +13,10 @@
       var lowerCase = false;
       var upperCase = false;
       var playButton = document.querySelector("#play");
-      var resultingPassword = "Test One";
+      var result = '';
 
-      function gatherData() {  //Function #1
+//Function 1: Gather Data
+      function gatherData() {  
       do {
       passWordLength = prompt("Please provide a password length between 8-128 characters");
       }
@@ -27,25 +29,48 @@
         lowerCase = confirm("Do you want a lower Case letter?");
         
         upperCase = confirm("Do you want a Upper Case letter?");
-        displayPassword();   
+      }
+    // document.getElementById("myTextarea").value = passWordLength;
+//  /*Tester*/     makeNum(passWordLength);
+ //Function 2: Random Numbers
+ function rnGenerator() {
+    var pwHolder = document.getElementById("demo")
+    pwHolder = Math.floor((Math.random() * 121) + 8);
+ }
+//Function 3 - Special Character
+function makeid(Length) {
+    // var result           = '';
+    // var sCharacterChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = sCharacterChar.length;
+    for ( var i = 0; i < length; i++ ) {
+       resultingPassword += sCharacterChar.charAt(Math.floor(Math.random() * charactersLength));
+       return result;   
     }
+    // document.getElementById("myTextarea").value = result;
+    // return result;
+ 
+ //Function 4 - 
+//  var html = [];
+//  for (var i = 0, itemLen = items.length; i < itemLen; i++) {
+//      pwHolder.push(items[i].pwHolder)
+//  }
+//  output.innerHTML = html.join('');
 
-
+//Randoms to use 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
 // XXX will need to call on this function.
       function displayPassword() {  //last Function. Displays PW
-        document.getElementById("myTextarea").value = resultingPassword;
+        document.getElementById("myTextarea").value = result;
       }
- //Button 1     
+//Button 1     
       //Pressing 'Generate Password' is like pressing play
       playButton.addEventListener("click", gatherData);
       
     
       // We then loop through the favTVshows array...
-      for (var j = 0; j < passWord.length; j++) {
+    //   for (var j = 0; j < passWord.length; j++) {
 
-        // ...and create alert messages listing out each show in our array.
-        alert(favTVshows[j]);
-      }
-
-  
+    //     // ...and create alert messages listing out each show in our array.
+    // //     alert(favTVshows[j]);
+    //   }
+}
