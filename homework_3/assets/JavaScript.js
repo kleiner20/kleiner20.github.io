e59f074530f930455a059e1c9ae9356ca2af691d
Keith Kleinschmidt <kleiner_the_airliner.com>
2019-10-05 23:49:47 -0500

    Couldn't get it to display

--- homework_3/assets/JavaScript.js
+++ homework_3/assets/JavaScript.js
@@ -1,9 +1,10 @@
     //   var rNumbersArray = [];
-    var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
-    var sCharacterChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var upperCaseCh = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerCaseCh = "abcdefghijklmnopqrstuvwxyz";
+    var spCharacter = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var numCharacter = "0123456789";
     
-    var pwHolder = [];
+    var emptyString = "";
 
       var passWord = 0;
       var specialCharacter = false;
@@ -13,6 +14,8 @@
       var showText = document.getElementById("myTextarea");
       var playButton = document.querySelector("#play");
       var result = "oops";
+
+     
 
       function gatherData() {  
       do {
@@ -27,14 +30,92 @@
         lowerCase = confirm("Do you want a lower Case letter?");
         
         upperCase = confirm("Do you want a Upper Case letter?");
-      
-        showText.value = result + " there it is.";  //Temp here.  In function. Displays Text.
+
+        myFunction();
       }
 
-      //Pressing 'Generate Password' is like pressing play
-      playButton.addEventListener("click", gatherData);
+
+      
+    
+      //Functions start here
+      function myFunction(passWord) {
+      // var emptyString = "";
+      // var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+      // while (emptyString.length < passWord){
+      // if (emptyString.length < passWord && lowerCase === true) {
+
+      // for (var i = 0, n = lowerCaseCh.length; i < passWord && lowerCase === true; ++i){
+      // emptyString += lowerCaseCh[Math.floor(Math.random() * n)];
+      // } 
+      // for (var j = 0, o = upperCaseCh.length; j < passWord && upperCase === true; ++j){
+      // emptyString += upperCaseCh[Math.floor(Math.random() * o)];
+      // } 
+      // for (var k = 0, p = spCharacter.length; k < passWord && specialCharacter === true; ++k){
+      // emptyString += spCharacter[Math.floor(Math.random() * p)];
+      // } 
+      // for (var l = 0, q = numCharacter.length; l < passWord && numericNumber === true; ++l){
+      // emptyString += numCharacter[Math.floor(Math.random() * q)];
+      // } 
+      // return emptyString;
+          var length = passWord;
+              charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+              retVal = "";
+          for (var i = 0, n = charset.length; i < length; ++i) {
+              retVal += charset.charAt(Math.floor(Math.random() * n));
+          }
+          return retVal;
+      }
+      showText.innerHTML = myFunction(passWord);
+
+
+      // if (emptyString.length < passWord && upperCase === true) {
+
+      // emptyString += upperCaseCh[Math.floor(Math.random() * upperCaseCh.length)];
+      // }
+      // if (emptyString.length < passWord && specialCharacterv === true) {
+      // emptyString += spCharacter[Math.floor(Math.random() * spCharacter.length)];
+      // }
+      // if (emptyString.length < passWord && numericNumber === true) {
+      // emptyString += numCharacter[Math.floor(Math.random() * numCharacter.length)];
+      // }
+
+//       function display(){
+//       showText.innerHTML = emptyString;
+// }
+// console.log(emptyString);
+      
+      // function myFunction() {
+
+      //   if pwHolder || passWord { 
+      //   pwHolder = pwHolder + 
+      //   }
+      //   // var x = document.getElementById("demo")
+      //   else 
+      //   showText.innerHTML = Math.floor((Math.random() * 123) + 8);
+      // }
       
       
+      
+      
+    //   function makeid(length) {
+    //     var result           = '';
+    //     var characters       = 'abcdefghijklmnopqrstuvwxyz';
+    //     var charactersLength = characters.length;
+    //     for ( var i = 0; i < length; i++ ) {
+    //        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    //     }
+    //     return result;
+        
+    //  }
+    //  showText.textcontent = result;
+          //Pressing 'Generate Password' is like pressing play
+          playButton.addEventListener("click", gatherData);
+
+
+
+    //  showText.innerHTML = result;  //Temp here.  In function. Displays Text.
+    //  console.log(makeid(5));
       
       
       
@@ -43,4 +124,5 @@
 
       //   // ...and create alert messages listing out each show in our array.
       //   alert(favTVshows[j]);
-      // }
+      // 
+      
