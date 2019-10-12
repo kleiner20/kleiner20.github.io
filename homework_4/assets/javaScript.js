9b9197543c399e8d1b0c2001376751ebf85d3a8a
Keith Kleinschmidt <kleiner_the_airliner.com>
2019-10-12 01:27:05 -0500

    Commit midnight

--- homework_4/assets/javaScript.js
+++ homework_4/assets/javaScript.js
@@ -1,55 +1,120 @@
-var quizContainer = document.getElementById('quiz');
+//button on index page starts quiz
+var quizContainer = document.getElementById('quiz'); 
+//defines quesion
 var resultsContainer = document.getElementById('results');
-var quizButton = document.getElementById('start'); //done
+var submitButton = document.getElementById('submit'); 
+var timeGauge = document.getElementById("timeGauge");
+var progress = document.getElementById("progress");
+
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+
+var runningQuestion = 0;
+var correctAnswers = 0;  //undefined
+var quizOver = false;    //undefined
 
 
+var output = [];  //not in example
    //Questions and answers saved in an object
-   var questions = [
-    {
-      title: "The best team in the NFL is...:",
-      choices: ["Cowboys", "Vikings", "Packers", "Steelers"],
-      answer: "Packers"
+   var questions = [{
+      question: "The best team in the NFL is...:",
+      choices: {
+        a: "Cowboys", 
+        b: "Vikings", 
+        c: "Packers", 
+      },
+      correctAnswer: "Packers"
+    },{
+      question: "Where is the best place to scooter or ride your bike?",
+      choices: {
+        a: "street", 
+        b: "sidewalk", 
+        c: "jumps", 
+      },
+      correctAnswer: "sidewalk"
+    }, {
+        question: "Best activity on a rainy day?",
+        choices: {
+        a:"movie",
+        b: "boardgames", 
+        c: "reading",
+        },
+        correctAnswer: "boardgames"
+      }, {
+        question: "What is the best kind of choclate?",
+        choices: {
+        a: "white", 
+        b: "milk", 
+        c: "dark", 
     },
-    {
-      title: "Where is the best place to scooter or ride your bike?",
-      choices: ["street", "sidewalk", "jumps", "Minnesota"],
-      answer: "sidewalk"
-    },
-    {
-        title: "Best activity on a rainy day?",
-        choices: ["movie", "boardgames", "reading", "nerfwars"],
-        answer: "boardgames"
-      },
-      {
-        title: "What is the best kind of choclate?",
-        choices: ["white", "milk", "dark", "orange"],
-        answer: "dark"
-      },
-      {
-        title: "Who is the funniest comedia?",
-        choices: ["Martin Lawrence", "Kevin Hart", "Dave Chapelle", "Dane Cook"],
-        answer: "Kevin Hart"
-      },
+        correctAnswer: "dark"
+      },{
+        question: "Who is the funniest comedia?",
+        choices: {
+          a: "Martin Lawrence", 
+          b: "Kevin Hart", 
+          c: "Dave Chapelle", 
+        },
+        correctAnswer: "Kevin Hart"
+      }
   ];
+
+
+
+  $(document).ready(function () {
+
+// render a question
+function renderQuestion(){
+  var q = questions[runningQuestion];
+  
+  quiz.innerHTML = "<p>"+ q.question +"</p>";
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+}
+
+
+
+
+
+
 
    //link to highscores page
    function visitScores(){
     location.href='highScores.html';
    }
+   function quizPage(){
+    location.href='quiz.html';
+    renderQuestion();
+   }
+   //Building the page
    function buildQuiz(){
-   }
-   function startQuiz(){
-   location.href='quiz.html';
-   }
-   // display quiz right away
-//    buildQuiz();
 
-    // on submit, show results
-    quizButton.addEventListener('click', startQuiz);
-   
-   // on submit, show results
-   submitButton.addEventListener('click', showResults);
-
-
-
+       var output = [];
+       myQuestions.forEach(currentQuestion, questionNumber);
+       var answers = [];
+       for(letter in currentQuestion.answers){
+         //html radio button
+        answers.push(
+            <label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>
+          );
+        }
+        output.push( 
+          '<div class="question"> $(currentQuestion.question) </div>;
+          <div class="answers"> ${answers.join('')} </div>'
+        );
+        }
+        );
+       
+       function showResults(){
+       }
   
+    // on submit, show results
+    submitButton.addEventListener('click', showResults());
+
+ 
