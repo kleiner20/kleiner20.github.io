1f3443f45c21deee87f62ea09bb1b16c0d68427a
Keith Kleinschmidt <kleiner_the_airliner.com>
2019-10-13 21:46:11 -0500

    Quiz page centered

--- homework_4/assets/javaScript.js
+++ homework_4/assets/javaScript.js
@@ -2,32 +2,34 @@ var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var quizButton = document.getElementById('start'); //done
 
+var question = document.getElementById('#question'); 
+
 
    //Questions and answers saved in an object
    var questions = [
     {
       title: "The best team in the NFL is...:",
-      choices: ["Cowboys", "Vikings", "Packers", "Steelers"],
+      choices: ["Cowboys", "Vikings", "Packers",],
       answer: "Packers"
     },
     {
       title: "Where is the best place to scooter or ride your bike?",
-      choices: ["street", "sidewalk", "jumps", "Minnesota"],
+      choices: ["street", "sidewalk", "jumps"],
       answer: "sidewalk"
     },
     {
         title: "Best activity on a rainy day?",
-        choices: ["movie", "boardgames", "reading", "nerfwars"],
+        choices: ["movie", "boardgames", "reading"],
         answer: "boardgames"
       },
       {
         title: "What is the best kind of choclate?",
-        choices: ["white", "milk", "dark", "orange"],
+        choices: ["white", "milk", "dark"],
         answer: "dark"
       },
       {
         title: "Who is the funniest comedia?",
-        choices: ["Martin Lawrence", "Kevin Hart", "Dave Chapelle", "Dane Cook"],
+        choices: ["Martin Lawrence", "Kevin Hart", "Dave Chapelle"],
         answer: "Kevin Hart"
       },
   ];
@@ -50,6 +52,14 @@ var quizButton = document.getElementById('start'); //done
    // on submit, show results
    submitButton.addEventListener('click', showResults);
 
-
-
+   var i;
+   for (i = 0; i < questions.length; i++) {
+     question.innerHTML = questions.title[i];
+     answer1.innerHTML = questions.title.choices[i];
+     i + 1;
+     answer2.innerHTML = questions.title[i];
+     i + 1
+     answer3.innerHTML = questions.title[i];
+     
+   }
   
