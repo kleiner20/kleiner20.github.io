a414eecd750baac64efe7e47a702e62c9ed0d1c4
Keith Kleinschmidt <kleiner_the_airliner.com>
2019-11-16 23:58:26 -0600

    late night commit

--- homework_8/index.js
+++ homework_8/index.js
@@ -1,8 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-const getGithubUser = require('get-github-user');
-const env = require('dotenv').config()
 const axios = require("axios");
 
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -11,24 +9,40 @@ function promptUser() {
   return inquirer.prompt([
     {
       type: "input",
-      username: "github",
-      message: "What is your GitHub Username"
+      name: "github",
+      message: "Enter your GitHub Username"
     },
     {
       type: "input",
-      color: "color",
+      name: "color",
       message: "What is your favorite color?"
     },
-    
-    // .then(function({ username }) {
-    //   const queryUrl = `https://api.github.com/users/${username}`;
-    
-    //   axios.get(queryUrl).then(function(res) {
-    //     const repoNames = res.data.map(function(repo) {
-    //       return repo.name;
+    {
+      type: "input",
+      name: "hobby",
+      message: "What is your favorite hobby?"
+    },
+    {
+      type: "input",
+      name: "food",
+      message: "What is your favorite food?"
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL."
+    }
   ]);
 }
+function getProfile(answers){
 
+  const queryUrl = "https://api.github.com/users/" + answers.github;
+
+  console.log(queryUrl);
+ 
+  var githubToken = process.env.GITHUB_TOKEN;
+ 
+ };
 function generateHTML(answers) {
   return `
 <!DOCTYPE html>
@@ -42,8 +56,8 @@ function generateHTML(answers) {
 <body>
   <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Hi! My GitHub username is ${answers.username}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    
     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
     <ul class="list-group">
       <li class="list-group-item">My GitHub username is ${answers.github}</li>
@@ -53,33 +67,7 @@ function generateHTML(answers) {
 </div>
 </body>
 </html>`;
-};
-
-
-function getProfile(answers){
- 
- 
- var githubToken = process.env.GITHUB_TOKEN;
-
-};
-const queryUrl = `https://api.github.com/users/${answers.username}`;
-
-console.log(`${username}`);
-
- //axios.get('api.github.com' + answers.github, {auth:githubToken}).then(function (response) {
-    // handle success
-    // console.log(response);
-
-    // //this is where you'd make the actual html
-    
-    //   const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-    
-      axios.get(queryUrl).then(function(response) {
-        // const repoNames = res.data.map(function(repo) 
-          // return response.name;
-          console.log(response.name);
-
-      });
+}
 
 async function init() {
   console.log("hi");
@@ -87,7 +75,15 @@ async function init() {
     const answers = await promptUser();
 
     const html = generateHTML(answers);
-    const profile = getProfile(answers);
+
+    // const profile = getProfile(answers);
+    const queryUrl = "https://api.github.com/users/" + answers.github;
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(function(response) {
+    console.log(response);
+
+    });  
 
     await writeFileAsync("index.html", html);
 
@@ -95,6 +91,6 @@ async function init() {
   } catch (err) {
     console.log(err);
   }
-};
+}
 
 init();
